185bb3fda92a2478e55537de817c5b31b7e36f93
Dejon <Dejoneltahan141@gmail.com>
2026-06-17 08:36:58 +1200

    bench: 446382

--- search.c
+++ search.c
@@ -12,6 +12,10 @@
 #define MAX_DEPTH 200
 #define MAX_GAME_PLY 2048
 
+void clear_ordering_tables(void)
+{
+}
+
 static void move_to_uci(uint16_t move, char *buf)
 {
     int from = (move >> 6) & 0x3F;
@@ -63,6 +67,31 @@ static int score_to_tt(int score, int ply)
         return score - ply;
     return score;
 }
+
+MoveList ordermoves(Position *board, MoveList *move_list, int ply, uint16_t tt_move)
+{
+    (void)board;
+    (void)ply;
+
+    MoveList ordered = *move_list;
+
+    if (!tt_move)
+        return ordered;
+
+    for (unsigned int i = 0; i < ordered.offset; i++)
+    {
+        if (ordered.movelist[i] == tt_move)
+        {
+            uint16_t tmp = ordered.movelist[0];
+            ordered.movelist[0] = ordered.movelist[i];
+            ordered.movelist[i] = tmp;
+            break;
+        }
+    }
+
+    return ordered;
+}
+
 
 searchOutput search(Position *board, int depth, int ply, int alpha, int beta,
                     stopConditions *stop, PVLine *pv)
@@ -116,6 +145,7 @@ searchOutput search(Position *board, int depth, int ply, int alpha, int beta,
 
     MoveList move_list = {0};
     legalMoveGen(board, &move_list);
+    move_list = ordermoves(board, &move_list, ply, tt_move);
 
     if (move_list.offset == 0)
     {
