41fce3e6d6e5ef385e4c9ec65c191f10b557ee26
DeeZ <dejoneltahan141@gmail.com>
2026-07-11 02:38:50 +1200

    bench:632780

--- search.c
+++ search.c
@@ -350,7 +350,7 @@ searchOutput search(Position *board, int depth, int ply, int alpha, int beta,
                    depth, mv, i + 1);
             fflush(stdout);
         }
-        if (depth <= 1 &&
+        if (depth <= 4 &&
             !in_check &&
             !is_mate_score(alpha) &&
             !is_mate_score(beta))
