e048c456375d03ed29e5b3ab5b945982f5665d90
DeeZ <dejoneltahan141@gmail.com>
2026-09-10 23:23:16 +1200

    bench:2943660

--- search.c
+++ search.c
@@ -606,9 +606,10 @@ searchOutput search(Position *board, int depth, int ply, int alpha, int beta,
 
         if (!root_node &&
             !in_check &&
-            depth <= 3 &&
+            depth <= 8 &&
             !is_capture &&
             !is_killer &&
+            !is_promotion &&
             (int)i >= (improving ? 24 : 16))
         {
             continue;
