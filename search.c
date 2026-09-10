583d41cc3a715b3b85ccb3d494c4b8c520c80004
DeeZ <dejoneltahan141@gmail.com>
2026-09-11 09:27:14 +1200

    bench:5568165

--- search.c
+++ search.c
@@ -501,7 +501,7 @@ searchOutput search(Position *board, int depth, int ply, int alpha, int beta,
     if (in_check && depth < MAX_DEPTH)
         depth++;
 
-    if (depth >= 4 && tt_move == 0 && !in_check)
+    if (depth >= 4 && !root_node && tt_move == 0 && !in_check)
     {
         depth--;
     }
@@ -642,7 +642,7 @@ searchOutput search(Position *board, int depth, int ply, int alpha, int beta,
                 continue;
         }
 
-        if (depth <= 3 && !in_check && !is_mate_score(alpha) && !is_mate_score(beta))
+        if (depth <= 3  && !root_node && !in_check && !is_mate_score(alpha) && !is_mate_score(beta))
         {
             int futility_margin = 120 + 90 * depth;
             if (static_eval + futility_margin <= alpha)
