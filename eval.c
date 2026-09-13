6d636e148a81ddc577a6459819620ae10e1be7ea
Dejon <Dejoneltahan141@gmail.com>
2026-09-14 11:44:35 +1200

    bench:2930884

--- eval.c
+++ eval.c
@@ -18,7 +18,7 @@
 INCBIN(EvalFile, EVALFILE);
 
 #define NNUE_INPUT   768
-#define NNUE_HL      128
+#define NNUE_HL      256
 #define NNUE_QA      255
 #define NNUE_QB      64
 #define NNUE_SCALE   400
