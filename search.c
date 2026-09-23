17d7f57a18431af86fa72da9a90daabd99ff56b5
Dejon <Dejoneltahan141@gmail.com>
2026-09-23 20:42:52 +1200

    bench:3340769

--- search.c
+++ search.c
@@ -854,8 +854,7 @@ searchOutput search(Position *board, int depth, int ply, int alpha, int beta,
             !in_check &&
             is_capture &&
             !is_promotion &&
-            depth <= 7 &&
-            (int)i >= 4 &&
+            depth <= 8 &&
             move != tt_move)
         {
             int from = move_from(move);
