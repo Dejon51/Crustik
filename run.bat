89dd26d1e00e34072b11fc2ed1648134579bb996
DeeZ <dejoneltahan141@gmail.com>
2026-09-27 01:50:08 +1200

    bench:3094129

--- run.bat
+++ run.bat
@@ -7,6 +7,6 @@ if "%~1"=="" (
     set EVALFILE=%~1
 )
 
-gcc main.c play.c lmath.c eval.c uci.c fen.c search.c zobrist.c tt.c bench.c text.c -lm -DNDEBUG -O3 -flto -march=native -Wall -Wextra -Wshadow -DEVALFILE=\"%EVALFILE%\" -o crustik
+gcc main.c play.c lmath.c eval.c uci.c fen.c search.c zobrist.c tt.c bench.c text.c magics.c -lm -DNDEBUG -O3 -flto -march=native -Wall -Wextra -Wshadow -DEVALFILE=\"%EVALFILE%\" -o crustik
 
 endlocal
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 EVALFILE="${EVALFILE:-quant384hl.bin}"
 
-gcc main.c play.c lmath.c eval.c uci.c fen.c search.c zobrist.c tt.c bench.c text.c -lm -DNDEBUG -O3 -flto -march=native -Wall -Wextra -Wshadow -DEVALFILE=\"$EVALFILE\" "$@" -o crustik
+gcc main.c play.c lmath.c eval.c uci.c fen.c search.c zobrist.c tt.c bench.c text.c magics.c -lm -DNDEBUG -O3 -flto -march=native -Wall -Wextra -Wshadow -DEVALFILE=\"$EVALFILE\" "$@" -o crustik
